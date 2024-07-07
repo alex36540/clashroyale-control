1f2a75a316cb8b68bfd39525f15cef26ba373d09
Alexander Lee <alex36540@gmail.com>
2024-07-07 11:29:43 -0400

    increased speed dramatically

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import pyautogui
-from pynput import keyboard
+from pynput import keyboard, mouse
 
+# Constants for mouse positions
 CARD_1_X = 853
 CARD_2_X = 958
 CARD_3_X = 1066
@@ -8,41 +8,45 @@ CARD_4_X = 1173
 
 Y_VAL = 920
 
+mouse_ctrl = mouse.Controller()
+
 def on_press(key):
+    # Quit case
     if key == keyboard.Key.esc:
         raise Exception(key)
     
-    mouseX, mouseY = pyautogui.position()
+    original_pos = mouse_ctrl.position
 
     try:
         key_char = key.char
 
+        # Move mouse to correct card
         match key_char:
             case '1':
-                pyautogui.moveTo(CARD_1_X, Y_VAL)
-                pyautogui.click()
-                pyautogui.moveTo(mouseX, mouseY)
+                mouse_ctrl.position = (CARD_1_X, Y_VAL)
             case '2':
-                pyautogui.moveTo(CARD_2_X, Y_VAL)
-                pyautogui.click()
-                pyautogui.moveTo(mouseX, mouseY)
+                mouse_ctrl.position = (CARD_2_X, Y_VAL)
             case '3':
-                pyautogui.moveTo(CARD_3_X, Y_VAL)
-                pyautogui.click()
-                pyautogui.moveTo(mouseX, mouseY)
+                mouse_ctrl.position = (CARD_3_X, Y_VAL)
             case '4':
-                pyautogui.moveTo(CARD_4_X, Y_VAL)
-                pyautogui.click()
-                pyautogui.moveTo(mouseX, mouseY)
+                mouse_ctrl.position = (CARD_4_X, Y_VAL)
             case _:
                 print('Key {0} pressed'.format(key))
                 return
-    except:
-        print("invalid key pressed")
+        
+        # Click card and move back
+        mouse_ctrl.click(mouse.Button.left)
+        mouse_ctrl.position = original_pos
+
+    except Exception as e:
+        if key == keyboard.Key.esc:
+            print('Exiting program')
+        else:
+            print(e)
 
 def main():
     while True:
-        # start keyboard listener (blocking)
+        # start keyboard listener (blocking) and link on_press fn
         with keyboard.Listener(on_press=on_press) as listener:
             listener.join()
 
